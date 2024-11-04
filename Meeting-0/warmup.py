2a5bb8194e3064847b9852e3c1a3298814f36e81
Sukant Ghosh <sukantghosh@Sukants-MacBook-Air.local>
2024-11-04 20:02:53 +0000

    Warmup

---
+++ Meeting-0/warmup.py
@@ -0,0 +1,19 @@
+
+
+# You’re an alien. 
+# You learned 2 things on Earth: random number generation, basic geometry. 
+# Weird number: pi. How find out the value of pi?
+
+# Solution : 
+# Generate 1000 pairs(x,y) within [-1,1] square
+# Calculate d(x,y) < 1:draw a circle inside the square
+
+# Group of friends (n)
+# We want to find average salary of group
+# No one wants to disclose true salary.
+# How can you help?
+
+# Solution : 
+# Add a random number to salary, pass it to next and ask them to add their salary
+
+
